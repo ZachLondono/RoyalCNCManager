e628f0c149abec8052bc9f8721b62c08f010dd1b
ZachLondono <zachary.londono5@gmail.com>
2021-10-26 18:57:18 -0400

    Added settings view to set configuration paths

---
+++ RoyalCNCTracker/Settings.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace RoyalCNCTracker {
+	partial class Settings {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.SaveBtn = new System.Windows.Forms.Button();
+			this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.OutputPathText = new System.Windows.Forms.TextBox();
+			this.DataSourceText = new System.Windows.Forms.TextBox();
+			this.TrackingText = new System.Windows.Forms.TextBox();
+			this.EditOutputButton = new System.Windows.Forms.Button();
+			this.EditSourceBtn = new System.Windows.Forms.Button();
+			this.EditTrackingBtn = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.EditImageBtn = new System.Windows.Forms.Button();
+			this.ImageText = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.EditSinglesBtn = new System.Windows.Forms.Button();
+			this.SinglesText = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// SaveBtn
+			// 
+			this.SaveBtn.Location = new System.Drawing.Point(66, 329);
+			this.SaveBtn.Name = "SaveBtn";
+			this.SaveBtn.Size = new System.Drawing.Size(261, 32);
+			this.SaveBtn.TabIndex = 0;
+			this.SaveBtn.Text = "Save";
+			this.SaveBtn.UseVisualStyleBackColor = true;
+			this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(108, 15);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Local Patterns Path";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 69);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(121, 15);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Data Source Database";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 122);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(123, 15);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Job Tracking Database";
+			// 
+			// OutputPathText
+			// 
+			this.OutputPathText.Location = new System.Drawing.Point(13, 32);
+			this.OutputPathText.Name = "OutputPathText";
+			this.OutputPathText.Size = new System.Drawing.Size(261, 23);
+			this.OutputPathText.TabIndex = 4;
+			// 
+			// DataSourceText
+			// 
+			this.DataSourceText.Location = new System.Drawing.Point(12, 87);
+			this.DataSourceText.Name = "DataSourceText";
+			this.DataSourceText.Size = new System.Drawing.Size(261, 23);
+			this.DataSourceText.TabIndex = 5;
+			// 
+			// TrackingText
+			// 
+			this.TrackingText.Location = new System.Drawing.Point(13, 147);
+			this.TrackingText.Name = "TrackingText";
+			this.TrackingText.Size = new System.Drawing.Size(261, 23);
+			this.TrackingText.TabIndex = 6;
+			// 
+			// EditOutputButton
+			// 
+			this.EditOutputButton.Location = new System.Drawing.Point(308, 32);
+			this.EditOutputButton.Name = "EditOutputButton";
+			this.EditOutputButton.Size = new System.Drawing.Size(75, 23);
+			this.EditOutputButton.TabIndex = 7;
+			this.EditOutputButton.Text = "Edit";
+			this.EditOutputButton.UseVisualStyleBackColor = true;
+			this.EditOutputButton.Click += new System.EventHandler(this.EditOutputButton_Click);
+			// 
+			// EditSourceBtn
+			// 
+			this.EditSourceBtn.Location = new System.Drawing.Point(308, 87);
+			this.EditSourceBtn.Name = "EditSourceBtn";
+			this.EditSourceBtn.Size = new System.Drawing.Size(75, 23);
+			this.EditSourceBtn.TabIndex = 8;
+			this.EditSourceBtn.Text = "Edit";
+			this.EditSourceBtn.UseVisualStyleBackColor = true;
+			this.EditSourceBtn.Click += new System.EventHandler(this.EditSourceBtn_Click);
+			// 
+			// EditTrackingBtn
+			// 
+			this.EditTrackingBtn.Location = new System.Drawing.Point(308, 146);
+			this.EditTrackingBtn.Name = "EditTrackingBtn";
+			this.EditTrackingBtn.Size = new System.Drawing.Size(75, 23);
+			this.EditTrackingBtn.TabIndex = 9;
+			this.EditTrackingBtn.Text = "Edit";
+			this.EditTrackingBtn.UseVisualStyleBackColor = true;
+			this.EditTrackingBtn.Click += new System.EventHandler(this.EditTrackingBtn_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// EditImageBtn
+			// 
+			this.EditImageBtn.Location = new System.Drawing.Point(307, 201);
+			this.EditImageBtn.Name = "EditImageBtn";
+			this.EditImageBtn.Size = new System.Drawing.Size(75, 23);
+			this.EditImageBtn.TabIndex = 12;
+			this.EditImageBtn.Text = "Edit";
+			this.EditImageBtn.UseVisualStyleBackColor = true;
+			this.EditImageBtn.Click += new System.EventHandler(this.EditImageBtn_Click);
+			// 
+			// ImageText
+			// 
+			this.ImageText.Location = new System.Drawing.Point(12, 202);
+			this.ImageText.Name = "ImageText";
+			this.ImageText.Size = new System.Drawing.Size(261, 23);
+			this.ImageText.TabIndex = 11;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(11, 177);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(106, 15);
+			this.label4.TabIndex = 10;
+			this.label4.Text = "Image Source Path";
+			// 
+			// EditSinglesBtn
+			// 
+			this.EditSinglesBtn.Location = new System.Drawing.Point(308, 265);
+			this.EditSinglesBtn.Name = "EditSinglesBtn";
+			this.EditSinglesBtn.Size = new System.Drawing.Size(75, 23);
+			this.EditSinglesBtn.TabIndex = 15;
+			this.EditSinglesBtn.Text = "Edit";
+			this.EditSinglesBtn.UseVisualStyleBackColor = true;
+			this.EditSinglesBtn.Click += new System.EventHandler(this.EditSinglesBtn_Click);
+			// 
+			// SinglesText
+			// 
+			this.SinglesText.Location = new System.Drawing.Point(13, 266);
+			this.SinglesText.Name = "SinglesText";
+			this.SinglesText.Size = new System.Drawing.Size(261, 23);
+			this.SinglesText.TabIndex = 14;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(12, 241);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(120, 15);
+			this.label5.TabIndex = 13;
+			this.label5.Text = "Single Programs Path";
+			// 
+			// Settings
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(395, 373);
+			this.Controls.Add(this.EditSinglesBtn);
+			this.Controls.Add(this.SinglesText);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.EditImageBtn);
+			this.Controls.Add(this.ImageText);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.EditTrackingBtn);
+			this.Controls.Add(this.EditSourceBtn);
+			this.Controls.Add(this.EditOutputButton);
+			this.Controls.Add(this.TrackingText);
+			this.Controls.Add(this.DataSourceText);
+			this.Controls.Add(this.OutputPathText);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.SaveBtn);
+			this.Name = "Settings";
+			this.Text = "Settings";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button SaveBtn;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox OutputPathText;
+		private System.Windows.Forms.TextBox DataSourceText;
+		private System.Windows.Forms.TextBox TrackingText;
+		private System.Windows.Forms.Button EditOutputButton;
+		private System.Windows.Forms.Button EditSourceBtn;
+		private System.Windows.Forms.Button EditTrackingBtn;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button EditImageBtn;
+		private System.Windows.Forms.TextBox ImageText;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button EditSinglesBtn;
+		private System.Windows.Forms.TextBox SinglesText;
+		private System.Windows.Forms.Label label5;
+	}
+}
---
+++ RoyalCNCTracker/Settings.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoyalCNCTracker {
+	public partial class Settings : Form {
+
+		private Configuration config { get; set; }
+
+		public Settings() {
+			InitializeComponent();
+
+			config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			var settings = config.AppSettings.Settings;
+
+			var output = settings["Output"];
+			if (output is not null) OutputPathText.Text = output.Value;
+
+			var source = settings["Source"];
+			if (source is not null) DataSourceText.Text = source.Value;
+
+			var data = settings["Data"];
+			if (data is not null) TrackingText.Text = data.Value;
+
+			var images = settings["ImageSource"];
+			if (images is not null) ImageText.Text = images.Value;
+
+
+			var singles = settings["SinglesSource"];
+			if (singles is not null) SinglesText.Text = singles.Value;
+
+		}
+
+		private void EditOutputButton_Click(object sender, EventArgs e) {
+			DialogResult result = folderBrowserDialog.ShowDialog();
+			if (result == DialogResult.OK)
+				OutputPathText.Text = folderBrowserDialog.SelectedPath;
+		}
+
+		private void EditSourceBtn_Click(object sender, EventArgs e) {
+			var result = openFileDialog1.ShowDialog();
+			if (result == DialogResult.OK)
+				DataSourceText.Text = openFileDialog1.FileName;
+		}
+
+		private void EditTrackingBtn_Click(object sender, EventArgs e) {
+			var result = openFileDialog1.ShowDialog();
+			if (result == DialogResult.OK)
+				TrackingText.Text = openFileDialog1.FileName;
+		}
+
+		private void SaveBtn_Click(object sender, EventArgs e) {
+			
+			var settings = config.AppSettings.Settings;
+			settings.Remove("Output");
+			settings.Add("Output", OutputPathText.Text);
+
+			settings.Remove("Source");
+			settings.Add("Source", DataSourceText.Text);
+
+			settings.Remove("Data");
+			settings.Add("Data", TrackingText.Text);
+
+			settings.Remove("ImageSource");
+			settings.Add("ImageSource", ImageText.Text);
+
+			settings.Remove("SinglesSource");
+			settings.Add("SinglesSource", SinglesText.Text);
+
+			config.Save();
+		}
+
+		private void EditImageBtn_Click(object sender, EventArgs e) {
+			var result = folderBrowserDialog.ShowDialog();
+			if (result == DialogResult.OK)
+				ImageText.Text = folderBrowserDialog.SelectedPath;
+		}
+
+		private void EditSinglesBtn_Click(object sender, EventArgs e) {
+			var result = folderBrowserDialog.ShowDialog();
+			if (result == DialogResult.OK)
+				SinglesText.Text = folderBrowserDialog.SelectedPath;
+		}
+	}
+}
